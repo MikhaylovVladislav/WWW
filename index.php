785058537d30200858ab807b9819279fe24a4d97
MikhaylovVladislav <vladik.mikhaylov@list.ru>
2021-10-24 21:54:29 +0500

    Update index.php (add url)
    Добавил код, который выводит ссылка на страницы упражнений

--- index.php
+++ index.php
@@ -4,3 +4,12 @@
 $d=date("d.m.Y H:i");
 echo($d);
 ?>
+ <p> Ссылки на cтраницы:
+ 
+     <ul class = "menu">
+         <il> <a href="lab-1-1.php"> Упражнение 1-2 (файл: lab-1-1.php)</a> </il> 
+         <br>
+         <il> <a href="lab-1-2.php"> Упражнение 1-3 (файл: lab-1-2.php)</a> </il>
+         <br>
+         <il> <a href="lab-1-3.php"> Упражнение 1-4 (файл: lab-1-3.php)</a> </il>
+     </ul>
