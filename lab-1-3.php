d03546bf2e8be89edf8c356a8371add5b020df8a
MikhaylovVladislav <vladik.mikhaylov@list.ru>
2021-10-24 21:35:15 +0500

    Update lab-1-3.php (info var)
    Изменил код, в котором выводится на экран информации о переменной

--- lab-1-3.php
+++ lab-1-3.php
@@ -1,4 +1,8 @@
 <?php
 const NUM_E = 2.71828;
-echo('Число e равно = ' . NUM_E);
+$num_e1 = NUM_E;
+settype ($num_e1, double);
+echo'$num_e1 = ' . $num_e1 . ' - ' . gettype ($num_e1) . '<br>';
+
+
 ?>
