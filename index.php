05fddda5edc660bcbaeee6c36e5d2c26a8e1a50a
MikhaylovVladislav <vladik.mikhaylov@list.ru>
2021-10-25 15:23:28 +0500

    Update index.php (add url)
    Добавлены ссылки на другие страницы упражнений

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 $d=date("d.m.Y H:i");
 echo($d);
 ?>
- <p> Ссылки на cтраницы:
+ <p> Ссылки на cтраницы (1 часть):
  
      <ul class = "menu">
          <il> <a href="lab-1-1.php"> Упражнение 1-2 (файл: lab-1-1.php)</a> </il> 
@@ -14,4 +14,29 @@ echo($d);
          <il> <a href="lab-1-3.php"> Упражнение 1-4 (файл: lab-1-3.php)</a> </il>
 		 <br>
          <il> <a href="lab1-5.php"> Задача 1-1 (файл: lab1-5.php)</a> </il>
+		 <br>
+	 </ul>
+	  <p> Ссылки на cтраницы (2 часть):
+	<ul class = "menu">
+         <il> <a href="lab-2-1.php"> Упражнение 1-6 (файл: lab-2-1.php)</a> </il>
+		 <br>
+         <il> <a href="lab-2-2.php"> Упражнение 1-7 (файл: lab-2-2.php)</a> </il>
+		 <br>
+         <il> <a href="lab-2-3.php"> Упражнение 1-8 (файл: lab-2-3.php)</a> </il>
+		 <br>
+         <il> <a href="lab-2-4.php"> Упражнение 1-9 (файл: lab-2-4.php)</a> </il>
+		 <br>
+         <il> <a href="lab-2-5.php"> Упражнение 1-10 (файл: lab-2-5.php)</a> </il>
+		 <br>
+         <il> <a href="lab-2-6.php"> Упражнение 1-11 (файл: lab-2-6.php)</a> </il>
+		 <br>
+         <il> <a href="lab-2-7.php"> Упражнение 1-12 (файл: lab-2-7.php)</a> </il>
+		 <br>
+         <il> <a href="lab-2-8.php"> Упражнение 1-13 (файл: lab-2-8.php)</a> </il>
+		 <br>
+         <il> <a href="lab-2-9.php"> Упражнение 1-14 (файл: lab-2-9.php)</a> </il>
+		 <br>
+         <il> <a href="lab-2-10.php"> Задача 1-2 (файл: lab-2-10.php)</a> </il>
+		 <br>
+         <il> <a href="lab-2-11.php"> Задача 1-3 (5 вариант) (файл: lab-2-11.php)</a> </il>
      </ul>
