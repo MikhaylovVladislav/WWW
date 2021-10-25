88d1c61fc128df8718e8c3d000bd5ea776f2f3ff
MikhaylovVladislav <vladik.mikhaylov@list.ru>
2021-10-25 15:14:43 +0500

    Update lab-2-11.php (fixbag)
    Устранен повтор одинаковых чисел суммы квадратов

--- lab-2-11.php
+++ lab-2-11.php
@@ -1,20 +1,32 @@
 <?php
 $n = rand(0, 500);
 $isFind= false;
-$c = 0;
-$d = 0;
-for ($a=1; $a<=23; $a++){
+$isFindRepeat = false;
+$arrayRepeat = array();
+for ($a=1; $a<=23; $a++){ 
 	for ($b=1; $b<=23; $b++){
 		$p=($a*$a)+($b*$b);
-		if($p==$n and ($a!==$d and $b!=$c)){
-			$c = $a;
-			$d = $b;
+		if($p==$n) {
+			foreach ($arrayRepeat as $ar){
+			if (($ar == $a) or ($ar == $b)){
+			$isFindRepeat = true;
+			break;
+			} else {
+				
+				$isFindRepeat = false;
+		}
+		} 
+		if ($isFindRepeat==false){
+			$arrayRepeat [] = $a;
+			$arrayRepeat [] = $b;
 			echo "Число " . $n . " является суммой квадратов " . $a . " и " . $b . "<br>" ;
 			$isFind=true;
 		}
+		
 	}
 }
-if (isFind==false){
+}
+if ($isFind==false){
 	echo "Число " . $n . " не имеет представлений в видe суммы квадратов";
 }
 ?>
