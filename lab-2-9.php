b0e9b326ffa15b7e9ad262a81ef61b2ef8cf3405
MikhaylovVladislav <vladik.mikhaylov@list.ru>
2021-10-25 12:27:17 +0500

    Create and Update lab-2-9.php (Table multyplay))
    Вывод на экран таблицы умножения

---
+++ lab-2-9.php
@@ -0,0 +1,21 @@
+<HTML>
+<TITLE> Таблица умножения </TITLE>
+<BODY>
+<TABLE border = 1>
+<?php
+for ($i=1; $i<=10; $i++){
+	echo("<tr>");
+	for ($k=1; $k<=10; $k++){
+		echo ("<td align = center>");
+		$p=$i*$k;
+		echo($p);
+		echo("</td>");
+	}
+	echo("</tr>");
+}
+?>
+</TABLE>
+</BODY>
+</HTML>
+
+	
