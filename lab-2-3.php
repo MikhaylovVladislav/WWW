ed9ee766393b1b1e915bcf7c16f01fb8f9b80f9d
MikhaylovVladislav <vladik.mikhaylov@list.ru>
2021-11-15 14:26:11 +0500

    Создание и вывод ассоциативного массива
    lab-2-3

--- lab-2-3.php
+++ lab-2-3.php
@@ -10,5 +10,17 @@ switch ($a) {
 	default:
 	    echo 'Ответ не найден';
 }
+
+ echo '<br><br><br><br>'.'Лабораторная работа №2: ';
+echo '<br>'." ".'<br>'; // багованная строка
+
+$cust['cnum']="2001";
+$cust['cname']="Hoffman";
+$cust['city']="London";
+$cust['snum']="1001";
+
+foreach($cust as $key => $key_value) {
+  echo "Ключ = " . $key . ", Значение = " . $key_value .  '<br>';
+}
 ?>
 	
