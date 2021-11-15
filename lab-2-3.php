57530f0daa181af2d358bfcf21d5856cd731a06a
MikhaylovVladislav <vladik.mikhaylov@list.ru>
2021-11-15 21:47:26 +0500

    Ассоциативный массив

--- lab-2-3.php
+++ lab-2-3.php
@@ -14,6 +14,8 @@ switch ($a) {
  echo '<br><br><br><br>'.'Лабораторная работа №2: ';
 echo '<br>'." ".'<br>'; // багованная строка
 
+echo '<br>' . 'Ассоциативный массив: ';
+echo '<br>';
 $cust['cnum']="2001";
 $cust['cname']="Hoffman";
 $cust['city']="London";
@@ -23,11 +25,36 @@ foreach($cust as $key => $key_value) {
   echo "Ключ = " . $key . ", Значение = " . $key_value .  '<br>';
 }
 
+echo '<br>'.'Добавление нового элемента в ассоциативный массив';
 echo '<br>';
-
 $cust['rating']="100";
 foreach($cust as $key => $key_value) {
   echo "Ключ = " . $key . ", Значение = " . $key_value .  '<br>';
 }
+
+echo '<br>'.'Сортировка ассоциативного массива по значениям';
+echo '<br>';
+$custS=$cust;
+array_multisort($custS);
+foreach($custS as $key => $key_value) {
+  echo "Ключ = " . $key . ", Значение = " . $key_value .  '<br>';
+}
+
+echo '<br>'.'Сортировка ассоциативного массива по ключу';
+echo '<br>';
+$custKS=$cust;
+ksort($custKS);
+foreach($custKS as $key => $key_value) {
+  echo "Ключ = " . $key . ", Значение = " . $key_value .  '<br>';
+}
+
+echo '<br>'.'Сортировка ассоциативного массива с помощью функци sort()';
+echo '<br>';
+sort($cust);
+foreach($cust as $key => $key_value) {
+  echo "Ключ = " . $key . ", Значение = " . $key_value .  '<br>';
+}
+
+
 ?>
 	
