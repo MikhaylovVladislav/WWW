bd58ac89eadec0e09014a2b0f1405c1cd5917c30
MikhaylovVladislav <vladik.mikhaylov@list.ru>
2021-11-29 13:05:16 +0500

    выполнение упражнения 3-3

--- f3_p.php
+++ f3_p.php
@@ -1,5 +1,10 @@
 <?php
-echo ("Здравствуйте, " . $_POST["username_post"]);
-echo ("<hr>");
-echo ("Значение скрытого поля hideField равно " . $_POST["hideField_post"]);
+if($_POST["d"]=="plus"){
+$c=$_POST["a"]+$_POST["b"];
+echo ("сумма чисел = $c");
+}else{
+	$c=$_POST["a"]*$_POST["b"];
+	echo ("произведение чисел = $c");
+}
+echo ("<br> <a href='f3.html'> Вернуться назад </a>");
 ?>
