f4b390b1731838686d710fa4d1f256e7f9492357
MikhaylovVladislav <vladik.mikhaylov@list.ru>
2021-12-23 22:14:48 +0500

    Убрал дебаг

--- bd_student/gen_xls.php
+++ bd_student/gen_xls.php
@@ -26,7 +26,7 @@
  */
 
 /** Error reporting */
-error_reporting(E_ALL);
+//error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 date_default_timezone_set('Europe/London');
