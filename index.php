977a743273962e236c3949a11f0dbc896f94b296
MikhaylovVladislav <vladik.mikhaylov@list.ru>
2021-12-29 20:53:48 +0500

    Добавил ссылки в главную страницу

--- index.php
+++ index.php
@@ -88,14 +88,21 @@ echo($d);
 		 <br>
 	 </ul>
 	 
+	 <p> Ссылки на cтраницы (4 л.р.):
+ 
+     <ul class = "menu">
+         
+		 <il> <a href="bd_student/index.php"> Самостоятельная(bd_student) - для SprintHost </a> </il>
+		 <br>
+
+	 </ul>
+	 
 	  <p> Ссылки на cтраницы (5 л.р.):
  
      <ul class = "menu">
-         <il> <a href="bd_user/index.php"> Упражнение (bd_user) </a> </il>
-		 <br>
 		 <il> <a href="bd_student_local/Autorization/login.php"> Авторизоваться  (bd_student_local) - для локального сервера </a> </il>
 		 <br>
-<il> <a href="bd_student/Autorization/login.php"> Авторизоваться (bd_student) - для SprintHost  </a> </il>
+<il> <a href="bd_student_online/Autorization/login.php"> Авторизоваться (bd_student) - для SprintHost  </a> </il>
 		 <br>
          
 	 </ul>
