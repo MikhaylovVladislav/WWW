0a65f340a333d9ecc2bf2766ede2ee517b46faf5
MikhaylovVladislav <vladik.mikhaylov@list.ru>
2021-12-30 00:13:06 +0500

    Добавил удаление юзеров

---
+++ bd_student_local/delete_user.php
@@ -0,0 +1,11 @@
+
+<?php
+require "isAdmin.php";
+$linkmy=mysqli_connect("localhost","root") or die ("Невозможно
+подключиться к серверу");
+ mysqli_select_db($linkmy, "students") or die("Нет такой таблицы!");
+ $zapros="DELETE FROM users WHERE id_stud=" . $_GET['id'];
+ mysqli_query($linkmy,$zapros);
+ header("Location: index.php");
+ exit;
+?>
