4ee566af213b3e6680b830daa76607ba9aad0672
MikhaylovVladislav <vladik.mikhaylov@list.ru>
2021-12-29 22:32:58 +0500

    Настроил подключение к БД (таблице) )

--- bd_student_online/connectToDB.php
+++ bd_student_online/connectToDB.php
@@ -5,5 +5,5 @@ define ("PASS", "12345");
 define ("DB", "f0593353_students");
  
  $linkmy = @mysqli_connect(HOST, USER, PASS, DB) or die ('Не получилось из-за @mysqli_connect :(');
- mysqli_select_db($linkmy, "students") or die("Нет такой таблицы!");
+ mysqli_select_db($linkmy, "f0593353_students") or die("Нет такой таблицы!");
  ?>
