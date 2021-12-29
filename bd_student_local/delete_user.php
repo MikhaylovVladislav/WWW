a952e9e7d078f4d6e52ac82a8babd4bcadacaa0b
MikhaylovVladislav <vladik.mikhaylov@list.ru>
2021-12-30 00:16:19 +0500

    Настроил удаление юзеров

--- bd_student_local/delete_user.php
+++ bd_student_local/delete_user.php
@@ -4,7 +4,7 @@ require "isAdmin.php";
 $linkmy=mysqli_connect("localhost","root") or die ("Невозможно
 подключиться к серверу");
  mysqli_select_db($linkmy, "students") or die("Нет такой таблицы!");
- $zapros="DELETE FROM users WHERE id_stud=" . $_GET['id'];
+ $zapros="DELETE FROM users WHERE id=" . $_GET['id'];
  mysqli_query($linkmy,$zapros);
  header("Location: index.php");
  exit;
