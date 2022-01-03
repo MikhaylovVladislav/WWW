f0172c956518272ce8d9c26833edb27f5c6dcf9b
MikhaylovVladislav <vladik.mikhaylov@list.ru>
2022-01-03 20:40:38 +0500

    required  для полей некоторых

--- bd_student_online/new_zv.php
+++ bd_student_online/new_zv.php
@@ -5,12 +5,12 @@
 <body>
 <H2>Регистрация:</H2>
 <form action="save_new_zv.php" metod="get">
- Дата: <input name="data" size="20" type="date">
+ Дата: <input name="data" size="20" type="date" required >
  <?php
 $result=mysqli_query($linkmy, "SELECT id_stud, stud_fio, stud_faculty, stud_gr, stud_no_zk, stud_no_tel
 FROM stud "); // запрос на выборку сведений о пользователях
 echo "<br>";
-echo "  Студент: <select name='id_stud'>";
+echo "Студент: <select name='id_stud'>";
 
 		while ($row = mysqli_fetch_array($result)) {
 			    print "<p> <option value='" . $row['id_stud'] ."'>" . $row['stud_fio'] ."</option>";
@@ -20,14 +20,14 @@ echo "  Студент: <select name='id_stud'>";
 		
  $result=mysqli_query($linkmy, "SELECT * FROM subject"); // запрос на выборку сведений о пользователях
 echo "<br>";
-echo "  Предмет: <select name='id_subj'>";
+echo "Предмет: <select name='id_subj'>";
 		while ($row = mysqli_fetch_array($result)) {
 			    print "<p> <option value='" . $row['id_subj'] ."'>" . $row['subj_name'] ."</option>";	
 		}
 		echo "</select>";
 
 ?>
-<br>Оценка: <input name="grade" size="5" type="number" min="2" max="5">
+<br>Оценка: <input name="grade" size="5" type="number" min="2" max="5" required >
 <p><input name="add" type="submit" value="Добавить">
 <input name="b2" type="reset" value="Очистить"></p>
 </form>
